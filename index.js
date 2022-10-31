d9b693b7e8183c6fbd0e2b72e31812dbbe7c7fdf
Theodore John Josef <john.josef@aol.com>
2022-10-31 16:23:26 -0700

    commit tree dfd83a8b03f1b8ca90643da0db00a454439a4bc8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+let saturdayFun = (activity = "roller-skate") => (`This Saturday, I want to ${activity}!`);
+
+let mondayWork = (work = "go to the office") =>(`This Monday, I will ${work}.`);
+
+const wrapAdjective = (star = "*") => {
+    function result(emphatic) {
+        return `You are ${star}${emphatic}${star}!`;
+    };
+    debugger;
+    return result;
+}
